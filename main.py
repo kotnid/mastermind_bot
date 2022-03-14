8e3be956c1b7311e7a4ec232cd1af96bd315dbf9
kotnid <tkt0506tkt@gmail.com>
2022-03-14 21:48:45 +0800

    mastermind_bot : Add join function to main.py
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- main.py
+++ main.py
@@ -58,7 +58,24 @@ async def open(message):
 # join a room 
 @bot.message_handler(commands="join")
 async def join(message):
-    pass
+    check_ac(message)
+    if check_room(message):
+        await bot.reply(message , "You alread join a room :/")
+    else:
+        room_num = message.chat.replace("/join " , "")
+        myquery = {"_id" : room_num}
+
+        if room_db.find(myquery) == 0:
+            await bot.reply_to(message , "Invalid room number :< ")
+        else:
+            stats_db.update_one({"_id" : message.chat.id } , {"$set" : {"room" : room_num}})
+
+            data = room_db.find(myquery)
+            for id in data["players"]:
+                await bot.reply_to(id , "{} has joined the room {}".format(message.chat.username , room_num))
+            stats_db.update_one({"_id" : room_num } , {"$set" : {"room" : room_num}})
+
+            await bot.reply_to(message , f"Room {room_num} joined")
 
 # start the game
 @bot.message_handler(commands="start")
